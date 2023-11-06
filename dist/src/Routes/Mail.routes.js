45108358ce9a5c120e63521860e8537c55706b03
killianL <klecorf@normandiewebschool.fr>
2023-11-06 19:31:37 +0100

    cors in mail route

--- dist/src/Routes/Mail.routes.js
+++ dist/src/Routes/Mail.routes.js
@@ -2,7 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require('express');
 const app = express();
+const cors_middleware_1 = require("../middleware/cors.middleware");
 const EmailService_Controller_1 = require("../Controllers/EmailService.Controller");
 const router = express.Router();
-router.post('/send-email', EmailService_Controller_1.sendMail);
+router.post('/send-email', cors_middleware_1.corsMiddleware, EmailService_Controller_1.sendMail);
 exports.default = router;
