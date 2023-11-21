2b31e2b73cd1845e6f4e985a6ff6024c17348559
killianL <klecorf@normandiewebschool.fr>
2023-11-21 18:33:58 +0100

    edit cors

--- index.ts
+++ index.ts
@@ -16,7 +16,9 @@ const port = process.env.PORT || 5000;
 app.use(cors({
   origin: [
       "http://localhost:5173",
-      "http://vps-3aa18acd.vps.ovh.net:8080/"
+      "http://localhost:8080",
+      "http://vps-3aa18acd.vps.ovh.net:8080/",
+      "http://vps-3aa18acd.vps.ovh.net/"
   ],
   credentials: true
 }));
