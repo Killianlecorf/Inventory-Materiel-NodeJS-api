b6797775ad300349028cfac8f2f8a03b8bcba4ca
killian-lecorf <killianlecorf@outlook.fr>
2023-11-28 13:08:01 +0100

    try cors

--- index.ts
+++ index.ts
@@ -14,7 +14,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors({
   origin: [
-    "http://vps-3aa18acd.vps.ovh.net:8080"
+    "http://localhost:4173"
   ],
   credentials: true
 }));
--- src/middleware/cors.middleware.ts
+++ src/middleware/cors.middleware.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 
 export const corsMiddleware = (req: Request, res: Response, next: NextFunction) => {
-  res.header('Access-Control-Allow-Origin', 'http://vps-3aa18acd.vps.ovh.net:8080/');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:4173/');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); 
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   res.header('Access-Control-Allow-Credentials', 'true');
